81feaef341a40c9e554f610ec13087e17dadd8a2
William Li <wul@microsoft.com>
2019-05-31 14:40:29 -0700

    Reverts

--- test/dotnet-uninstall.Tests/MessageTests.cs
+++ test/dotnet-uninstall.Tests/MessageTests.cs
@@ -2,10 +2,16 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using FluentAssertions;
+using Xunit;
 
 namespace Microsoft.DotNet.Tools.Uninstall
 {
     public class MessageTests 
     {
+        [Fact]
+        public void It_should_have_correct_message()
+        {
+            Messages.message.Should().Be("Hello World!");
+        }
     }
 }
